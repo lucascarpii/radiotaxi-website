25844d0af70b5a2214fd189e38a505fde57e0a04
Lucas Fuentes <lucascarpii2002@gmail.com>
2024-11-19 16:34:06 -0300

    Instalar react y cambiar contenido de CTA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
 export default defineConfig({
 vite: {
     plugins: [tailwindcss()],
   },
     // add yur domain name here
    site: 'https://lexingtonthemes.com',
-  integrations: [sitemap()]
+  integrations: [sitemap(), react()]
 });
